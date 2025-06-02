0c08d4a9e841cb51272f42614382ca651911e492
gaaragaara386 <jerinjerin1268@gmail.com>
2025-06-03 00:27:15 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -82,8 +82,8 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
     message.send(":)")
   }
   }
-        if(event.reaction == "😠"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61574046213712"){
+        if(event.reaction == "🐸"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61565898444113"){
     message.unsend(event.messageID)
 }else{
     message.send(":)")
